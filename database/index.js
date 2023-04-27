a32597a4efca38fa933548ec5b7b6c1b51420f82
Daniel Yu <charlesyou1234567@gmail.com>
2023-04-28 00:39:31 +0800

    add mongodb altas

--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-const db = mongoose.connect("mongodb://127.0.0.1:27017/jotHive").then(()=>{
+const db = mongoose.connect("mongodb+srv://charlesyou1234567:rungname1234567@cluster0.sya60dl.mongodb.net/?retryWrites=true&w=majority").then(()=>{
   console.log("成功連結jotHive資料庫...")
 }).catch((e)=>{
   console.log(e)
